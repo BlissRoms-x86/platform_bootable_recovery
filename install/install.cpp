3ec9f418762bc45006f9558f3eaa9c1eaea18034
Joseph Annareddy <javelinanddart@gmail.com>
2023-08-16 12:04:56 -0400

    recovery: Bypass reboot prompt for sideload-auto-reboot
    When we want to automatically reboot after a sideload, it doesn't make
    much sense to try and prompt the user for a reboot into recovery.
    
    Change-Id: Id0195965362b62bf940caba1f83ffe12191a73c6

--- install/install.cpp
+++ install/install.cpp
@@ -565,7 +565,7 @@ static InstallResult TryUpdateBinary(Package* package, bool* wipe_cache,
   }
   if (package_is_ab) {
     ab_package_installed = true;
-    if (ask_to_ab_reboot(ui->GetDevice())) {
+    if (!ui->IsSideloadAutoReboot() && ask_to_ab_reboot(ui->GetDevice())) {
       reboot_to_recovery();
     }
   }
