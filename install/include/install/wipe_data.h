76f6cce954244c1e0ecd8ff7421760de370a47f3
hmtheboy154 <buingoc67@gmail.com>
2024-03-12 13:49:16 -0400

    Allow Wiping Data with rm -rf instead of formatting it
    For case like data directory instead of partition or data.img, we
    can't put /data under fstab, so it will result in failed or even
    crashing recovery.
    
    TWRP has a very nice feature that user can choose to remove all
    content of /data like using rm -rf instead of formatting the
    partition. We will take their code and take advantage of it.
    - Copy TWRP's removeDir() and modify it
    - Because we don't have a "safe" way to read fstab and check for
    userdata without being crashed if failed yet, we will temporary
    guard it with a property "sys.recovery.data_is_part"
    - If the property is false, switch to WipeDataDir(). If it's true,
    We can ask if user want to use remove dir or format the drive.
    
    Change-Id: I71f58c02989731c28c8e30f0f286d2a1c0bcbff4
    Signed-off-by: hmtheboy154 <buingoc67@gmail.com>

--- install/include/install/wipe_data.h
+++ install/include/install/wipe_data.h
@@ -29,5 +29,9 @@ bool WipeCache(RecoveryUI* ui, const std::function<bool()>& confirm);
 // Returns true on success.
 bool WipeData(Device* device, bool convert_fbe);
 
+int removeDir(RecoveryUI* ui, const std::string path, bool skipParent);
+
+bool WipeDataDir(Device* device, bool skipParent);
+
 // Returns true on success.
 bool WipeSystem(RecoveryUI* ui, const std::function<bool()>& confirm);
